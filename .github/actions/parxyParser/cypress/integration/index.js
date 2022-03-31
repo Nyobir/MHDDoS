81f41c295f4b4e3e98ec06edeb7d80d00c26b74d
yaroslav <2517>
2022-03-31 22:08:22 +0300

    first

--- .github/actions/parxyParser/cypress/integration/index.js
+++ .github/actions/parxyParser/cypress/integration/index.js
@@ -1,11 +1,15 @@
-context('Cypress proxy parsing', () => {
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+context('Cypress TodoMVC test', () => {
     beforeEach(() => {
-        cy.visit('https://hidemy.name/en/proxy-list/?type=hs#list')
+        // https://on.cypress.io/visit
+        cy.visit('https://hidemy.name/en/proxy-list/?type=h#list')
     })
 
-    it('parse_proxy', function () {
-        cy.get('a[href="/api/proxylist.txt?type=hs&out=plain&lang=en&utf"]').click()
-        cy.get('input[placeholder="Access code from email"]').type(Cypress.env('proxy_key'), {force: true})
+    it('test', function () {
+        cy.get('a[href="/api/proxylist.txt?type=h&out=plain&lang=en&utf"]').click()
+        cy.get('input[placeholder="Access code from email"]').type('555241928668692', {force: true})
         cy.get('button[type="submit"]').click({force: true})
         cy.get('textarea[class="input_text_field textarea ips"]').should('not.to.be.empty')
         cy.request({
@@ -13,7 +17,16 @@ context('Cypress proxy parsing', () => {
             url: `https://hidemy.name/api/proxylist.txt?out=plain&lang=en&utf&type=h`
         }).then(response => {
             let ave = response.body
-            cy.writeFile('prox.txt', ave)
+            //console.log(ave)
+            //core.setOutput("proxies", ave);
+            console.log(`The event payload: ${ave}`);
+            //cy.writeFile('prox.txt', ave)
         })
     })
+
+    // more examples
+    //
+    // https://github.com/cypress-io/cypress-example-todomvc
+    // https://github.com/cypress-io/cypress-example-kitchensink
+    // https://on.cypress.io/writing-your-first-test
 })
