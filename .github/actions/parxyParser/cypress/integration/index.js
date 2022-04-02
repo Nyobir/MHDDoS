b6e9483e004a291e8ed9d98f2b2a5ff8195cf8db
Nyobir <nyobir@gmail.com>
2022-04-02 18:27:39 +0300

    Https proxy to parser

--- .github/actions/parxyParser/cypress/integration/index.js
+++ .github/actions/parxyParser/cypress/integration/index.js
@@ -1,10 +1,10 @@
 context('Cypress proxy parsing', () => {
     beforeEach(() => {
-        cy.visit('https://hidemy.name/en/proxy-list/?type=h#list')
+        cy.visit('https://hidemy.name/en/proxy-list/?type=hs#list')
     })
 
     it('parse_proxy', function () {
-        cy.get('a[href="/api/proxylist.txt?type=h&out=plain&lang=en&utf"]').click()
+        cy.get('a[href="/api/proxylist.txt?type=hs&out=plain&lang=en&utf"]').click()
         cy.get('input[placeholder="Access code from email"]').type(Cypress.env('proxy_key'), {force: true})
         cy.get('button[type="submit"]').click({force: true})
         cy.get('textarea[class="input_text_field textarea ips"]').should('not.to.be.empty')
